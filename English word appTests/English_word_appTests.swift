74a2edf8ba4604c5196cd6036ad0d1bf7a238580
ryusei okada <gangtianliusheng85@gmail.com>
2026-03-23 21:14:28 +0900

    Initial Commit

---
+++ English word appTests/English_word_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  English_word_appTests.swift
+//  English word appTests
+//
+//  Created by 岡田瑠聖 on 2026/03/23.
+//
+
+import Testing
+
+struct English_word_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
